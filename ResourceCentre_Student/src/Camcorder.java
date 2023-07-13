ca5d1e3f055284a450e71ec065cdd9ff11235252
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2023-07-13 17:03:06 +0800

    1

--- ResourceCentre_Student/src/Camcorder.java
+++ ResourceCentre_Student/src/Camcorder.java
@@ -2,7 +2,6 @@
 public class Camcorder extends Item{
 private int opticalZoom;
 
-
 	public Camcorder(String assetTag, String description, int opticalZoom) {
 		super(assetTag, description);
 		this.opticalZoom = opticalZoom;
